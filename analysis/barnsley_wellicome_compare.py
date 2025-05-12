7d167e86f36ce598c674f82be1a5943d1cf13e8f
Kiley McClure <kiley147@mit.edu>
2025-05-12 12:21:45 -0500

    Fixed V'0 calculation

---
+++ analysis/barnsley_wellicome_compare.py
@@ -0,0 +1,240 @@
+import analysis_utils as au
+from pathlib import Path
+import os
+import math
+import padeopsIO as pio
+# from pathlib import Path
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+import numpy as np
+from padeopsIO import turbine
+
+#Data and Path Stuff
+data_path = Path(au.DATA_PATH)
+#Uncorrected
+sim1_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0000")
+sim1 = pio.BudgetIO("Data/U_0017_Files/Sim_0000", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim2_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0001")
+sim2 = pio.BudgetIO("Data/U_0017_Files/Sim_0001", padeops = True, runid = 1, normalize_origin = "turbine") 
+
+sim3_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0002")
+sim3 = pio.BudgetIO("Data/U_0017_Files/Sim_0002", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim4_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0003")
+sim4 = pio.BudgetIO("Data/U_0017_Files/Sim_0003", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim5_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0004")
+sim5 = pio.BudgetIO("Data/U_0017_Files/Sim_0004", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim6_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0005")
+sim6 = pio.BudgetIO("Data/U_0017_Files/Sim_0005", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim7_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0006")
+sim7 = pio.BudgetIO("Data/U_0017_Files/Sim_0006", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim8_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0007")
+sim8 = pio.BudgetIO("Data/U_0017_Files/Sim_0007", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim9_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0008")
+sim9 = pio.BudgetIO("Data/U_0017_Files/Sim_0008", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim10_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0009")
+sim10 = pio.BudgetIO("Data/U_0017_Files/Sim_0009", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim11_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0010")
+sim11 = pio.BudgetIO("Data/U_0017_Files/Sim_0010", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim12_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0011")
+sim12 = pio.BudgetIO("Data/U_0017_Files/Sim_0011", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim13_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0012")
+sim13 = pio.BudgetIO("Data/U_0017_Files/Sim_0012", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim14_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0013")
+sim14 = pio.BudgetIO("Data/U_0017_Files/Sim_0013", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim15_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0014")
+sim15 = pio.BudgetIO("Data/U_0017_Files/Sim_0014", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim16_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0015")
+sim16 = pio.BudgetIO("Data/U_0017_Files/Sim_0015", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim17_folder = os.path.join(au.DATA_PATH, "U_0017_Files/Sim_0016")
+sim17 = pio.BudgetIO("Data/U_0017_Files/Sim_0016", padeops = True, runid = 1, normalize_origin = "turbine")
+
+#Corrected
+sim1_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0000")
+sim1_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0000", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim2_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0001")
+sim2_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0001", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim3_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0002")
+sim3_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0002", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim4_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0003")
+sim4_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0003", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim5_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0004")
+sim5_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0004", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim6_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0005")
+sim6_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0005", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim7_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0006")
+sim7_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0006", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim8_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0007")
+sim8_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0007", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim9_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0008")
+sim9_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0008", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim10_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0009")
+sim10_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0009", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim11_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0010")
+sim11_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0010", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim12_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0011")
+sim12_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0011", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim13_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0012")
+sim13_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0012", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim14_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0013")
+sim14_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0013", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim15_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0014")
+sim15_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0014", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim16_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0015")
+sim16_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0015", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim17_folder_cor = os.path.join(au.DATA_PATH, "U_0018_Files/Sim_0016")
+sim17_cor = pio.BudgetIO("Data/U_0018_Files/Sim_0016", padeops = True, runid = 1, normalize_origin = "turbine")
+
+#Blocked, No Correction
+sim1_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0000")
+sim1_block = pio.BudgetIO("Data/B_0000_Files/Sim_0000", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim2_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0001")
+sim2_block = pio.BudgetIO("Data/B_0000_Files/Sim_0001", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim3_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0002")
+sim3_block = pio.BudgetIO("Data/B_0000_Files/Sim_0002", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim4_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0003")
+sim4_block = pio.BudgetIO("Data/B_0000_Files/Sim_0003", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim5_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0004")
+sim5_block = pio.BudgetIO("Data/B_0000_Files/Sim_0004", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim6_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0005")
+sim6_block = pio.BudgetIO("Data/B_0000_Files/Sim_0005", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim7_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0006")
+sim7_block = pio.BudgetIO("Data/B_0000_Files/Sim_0006", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim8_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0007")
+sim8_block = pio.BudgetIO("Data/B_0000_Files/Sim_0007", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim9_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0008")
+sim9_block = pio.BudgetIO("Data/B_0000_Files/Sim_0008", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim10_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0009")
+sim10_block = pio.BudgetIO("Data/B_0000_Files/Sim_0009", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim11_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0010")
+sim11_block = pio.BudgetIO("Data/B_0000_Files/Sim_0010", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim12_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0011")
+sim12_block = pio.BudgetIO("Data/B_0000_Files/Sim_0011", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim13_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0012")
+sim13_block = pio.BudgetIO("Data/B_0000_Files/Sim_0012", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim14_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0013")
+sim14_block = pio.BudgetIO("Data/B_0000_Files/Sim_0013", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim15_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0014")
+sim15_block = pio.BudgetIO("Data/B_0000_Files/Sim_0014", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim16_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0015")
+sim16_block = pio.BudgetIO("Data/B_0000_Files/Sim_0015", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim17_folder_block = os.path.join(au.DATA_PATH, "B_0000_Files/Sim_0016")
+sim17_block = pio.BudgetIO("Data/B_0000_Files/Sim_0016", padeops = True, runid = 1, normalize_origin = "turbine")
+
+#Blocked, Corrected
+sim1_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0000")
+sim1_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0000", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim2_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0001")
+sim2_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0001", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim3_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0002")
+sim3_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0002", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim4_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0003")
+sim4_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0003", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim5_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0004")
+sim5_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0004", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim6_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0005")
+sim6_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0005", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim7_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0006")
+sim7_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0006", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim8_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0007")
+sim8_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0007", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim9_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0008")
+sim9_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0008", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim10_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0009")
+sim10_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0009", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim11_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0010")
+sim11_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0010", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim12_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0011")
+sim12_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0011", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim13_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0012")
+sim13_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0012", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim14_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0013")
+sim14_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0013", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim15_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0014")
+sim15_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0014", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim16_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0015")
+sim16_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0015", padeops = True, runid = 1, normalize_origin = "turbine")
+
+sim17_folder_block_cor = os.path.join(au.DATA_PATH, "B_0001_Files/Sim_0016")
+sim17_block_cor = pio.BudgetIO("Data/B_0001_Files/Sim_0016", padeops = True, runid = 1, normalize_origin = "turbine")
+
+#Ct Primes
+Ctprime1 = sim1.ta[0].ct
+Ctprime2 = sim2.ta[0].ct
+Ctprime3 = sim3.ta[0].ct
+Ctprime4 = sim4.ta[0].ct
+Ctprime5 = sim5.ta[0].ct
+Ctprime6 = sim6.ta[0].ct
+Ctprime7 = sim7.ta[0].ct
+Ctprime8 = sim8.ta[0].ct
+Ctprime9 = sim9.ta[0].ct
+Ctprime10 = sim10.ta[0].ct
+Ctprime11 = sim11.ta[0].ct
+Ctprime12 = sim12.ta[0].ct
+Ctprime13 = sim13.ta[0].ct
+Ctprime14 = sim14.ta[0].ct
+Ctprime15 = sim15.ta[0].ct
+Ctprime16 = sim16.ta[0].ct
+Ctprime17 = sim17.ta[0].ct
+
