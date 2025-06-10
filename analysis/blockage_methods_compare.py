07943735589ddcf1212c6b6623e16d104d84830c
Kiley McClure <kiley147@mit.edu>
2025-06-10 13:13:25 -0500

    fixed naming convention for saving variables

---
+++ analysis/blockage_methods_compare.py
@@ -0,0 +1,10 @@
+import analysis_utils as au
+from pathlib import Path
+import os
+import math
+import padeopsIO as pio
+# from pathlib import Path
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+import numpy as np
+from padeopsIO import turbine
